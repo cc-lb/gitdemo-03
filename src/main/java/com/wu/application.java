fe02bedb1987ddc98e13203ce578c63ffce94de7
cc <ccs@.com>
2023-03-02 14:33:13 +0800

    first commit

---
+++ src/main/java/com/wu/application.java
@@ -0,0 +1,15 @@
+package com.wu;
+
+/**
+ * @Classname application
+ * @Description
+ * @Date 2023/2/28 19:47
+ * @Created by cc
+ */
+public class application {
+    public void main(String[] args){
+        System.out.println("dddd");
+        System.out.println("dddd11");
+        System.out.println("dddd22");
+    }
+}
